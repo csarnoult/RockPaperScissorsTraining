b06ccf2830e17f3579a11a3cb7d74ea334c723eb
csarnoult <chris.arnoult@icloud.com>
2019-12-09 12:49:55 -0800

    Initial Commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Chris Arnoult on 12/9/19.
+//  Copyright © 2019 Chris Arnoult. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
